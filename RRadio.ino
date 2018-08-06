35ac9dc47c5217a67efb1cc48595e13627028f9c
sintetico82 <sintetico82@gmail.com>
2018-08-06 17:26:53 +0200

    -- final version!

--- RRadio.ino
+++ RRadio.ino
@@ -1,3 +1,5 @@
+#include <ResponsiveAnalogRead.h>
+
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 #include <TEA5767N.h>
@@ -5,8 +7,10 @@
 TEA5767N radio = TEA5767N();
 LiquidCrystal_I2C lcd(0x27,20,4);
 
-int ledOn = 2;
-int analogPin = 7;
+
+const int ANALOG_PIN = A7;
+ResponsiveAnalogRead analog(ANALOG_PIN, true);
+
 
 float frequency = 0;
 float previousFrequency = 0;
@@ -15,8 +19,7 @@ int frequencyInt = 0;
 int val = 0; 
 int signalStrength = 0;
 
-char line0[16]; 
-char line1[16];
+
 
 void setup() {
 
@@ -25,39 +28,27 @@ void setup() {
   lcd.setCursor(3,0);
 	lcd.print("Rosa Radio");
 
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(ledOn,OUTPUT);
-  digitalWrite(ledOn,HIGH);
-  pinMode(analogPin,INPUT);
-
   radio.setMonoReception();
   radio.setStereoNoiseCancellingOn();
-
+  radio.setHighCutControlOn();
   Serial.begin(9600);
 }
 
 
 void loop() {
  
-  
-  val = analogRead(analogPin); 
-
-  
-
-  frequencyInt = map(val, 3, 1022, 8700, 10700); //Analog value to frequency from 87.0 MHz to 107.00 MHz 
+  analog.update();
+  val = analog.getValue();
+  frequencyInt = map(val, 0, 1023, 8700, 10700); //Analog value to frequency from 87.0 MHz to 107.00 MHz 
   float frequency = frequencyInt/100.0f;
 
   if(frequency - previousFrequency >= 0.1f || previousFrequency - frequency >= 0.1f)
   {
-
-    radio.selectFrequency(frequency);
+    radio.selectFrequencyMuting(frequency);
     previousFrequency = frequency;    
   }
-
-
-
   printFrequency(previousFrequency);
-  printSignalStrength();
+  printSignalStrength();  
   delay(100);                   
 }
 
